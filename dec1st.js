e2b28b08d106a78677988e2868328b8f0d9faaaa
LewRit <lewrit16@gmail.com>
2023-02-10 01:15:21 -0700

    committing advent

---
+++ dec1st.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+fs.readFile('/Users/mr.robot/Digitalcrafts/projects/advent-of-code/elfcalorie.txt', 'utf8',(err,data)=>{
+    if (err){
+        console.error(err);
+        return;
+    }
+    const elves=[];
+    const lines = data.split("\n");
+    var calorie = 0
+    for (let i=0; i<lines.length; i++){
+        const line=lines[i];
+        if (line === '') {
+            elves.push(calorie);
+            calorie = 0;
+        }
+        else {
+            calorie += parseInt(line)
+        }
+    }
+    const top=[];
+    total=0
+    for (let i = 0; i < 3; i++){
+        value = Math.max(...elves)
+        top.push(value)
+        elves.splice(elves.indexOf(value),1)
+        total += value;
+    };
+    console.log(total);
+});
